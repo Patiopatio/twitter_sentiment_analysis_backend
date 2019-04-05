a7629ba4feb44818866cea6c73c24c8a32411e7e
Patiopatio <buczkowska.p@gmail.com>
2019-04-05 16:25:04 +0200

    initial commit

---
+++ scripts/dependencies.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# using venv kernel
+pip install ipykernel;
+ipython kernel install --user --name=venv;
+# dependencies HERE
+pip install tweepy;
+pip install textblob;
+pip install flask;
+pip install flask-cors;
